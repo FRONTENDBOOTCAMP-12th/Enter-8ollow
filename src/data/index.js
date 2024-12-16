e9014786dc7aa659a7b1cbd5dbd60580d76ee33a
myeong-jae-hwi <audwognl@gmail.com>
2024-12-16 13:17:29 +0900

    [feat]: 랜덤 닉네임
    - 동물 데이터 생성
    - 형용사 데이터 생성
    - index 파일에 두 데이터를 이용해 랜덤으로 닉네임 생성

---
+++ src/data/index.js
@@ -0,0 +1,11 @@
+import { ADJECTIVE } from './adjective.js';
+import { ANIMALS } from './animals.js';
+
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+const randomAdjective = getRandomElement(ADJECTIVE);
+const randomAnimal = getRandomElement(ANIMALS);
+
+export default `${randomAdjective} ${randomAnimal}`;
