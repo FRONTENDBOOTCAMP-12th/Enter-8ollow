5cd3c850b30ab25e966efc4b0b4406e0d87398a6
myeong-jae-hwi <audwognl@gmail.com>
2024-12-17 16:37:55 +0900

    [Rename]: import 정리
    - 컴포넌트 관련 import main으로 묶음

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,5 @@
-import '/src/components/common';
-import '/src/components/signUp';
-
-// import '/src/components/Header';
-// import '/src/components/Header.css';
-
-import '/src/components/Main/RegionMenu/RegionMenu';
-import '/src/components/Main/RegionMenu/RegionMenu.css';
-
-import '/src/components/Main/SwiperComponent/SwiperComponent';
-
-import '/src/components/Main/StoryField/StoryField';
-
-import '/src/components/withUs/WithUs';
-import '/src/components/Board/BoardInfoItems/BoardInfoItems';
-import '/src/components/Board/BoardPerson/BoardPerson';
-import '/src/components/Board/BoardField/BoardField';
+import {} from '/src/components/Main/index.js';
+import {} from '/src/components/signUp/index.js';
+import {} from '/src/components/common/index.js';
+import {} from '/src/components/finished/finished.js';
+import {} from '/src/components/Board/index.js';
