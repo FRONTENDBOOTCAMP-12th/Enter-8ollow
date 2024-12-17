13d16f50f6e5327a27a8ae65ea68d8f17f23e9e0
akman12914 <rnlduadns98@naver.com>
2024-12-18 03:51:19 +0900

    [Modify] 스크롤 설정

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -1,5 +1,5 @@
 import { LitElement, html } from 'lit';
-import s from '/src/pages/board/board.css';
+import s from '/src/pages/board/board.css?inline';
 
 class Board extends LitElement {
   constructor() {
@@ -8,11 +8,10 @@ class Board extends LitElement {
   }
 
   render() {
-    return html` <style>
+    return html`
+      <style>
         ${s}
       </style>
-
-      <region-menu></region-menu>
       <div class="main-button-container">
         <main-button name="주제"></main-button>
         <main-button name="인기글"></main-button>
@@ -20,6 +19,7 @@ class Board extends LitElement {
         <main-button name="질의응답"> </main-button>
         <main-button name="자유게시판"></main-button>
       </div>
+
       <ul class="board-field-container">
         <li>
           <board-field
@@ -27,18 +27,19 @@ class Board extends LitElement {
             title="youtube 클론 프젝 같이 하실 분~!"
             location="응암제 3동"
             personCount="1/3"
-            gender="누구나참석가능"
+            gender="${this.description}"
             meetDay="오늘"
           ></board-field>
         </li>
-        <li><board-field text="질의응답">${this.description}</board-field></li>
+        <li><board-field text="질의응답"></board-field></li>
         <li><board-field text="질의응답"></board-field></li>
         <li><board-field text="질의응답"></board-field></li>
         <li><board-field text="같이해요"></board-field></li>
         <li><board-field text="자유게시판"></board-field></li>
       </ul>
-
-      <base-header></base-header>`;
+      <region-menu></region-menu>
+      <base-header></base-header>
+    `;
   }
 }
 
