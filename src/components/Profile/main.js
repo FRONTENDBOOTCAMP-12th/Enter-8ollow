c54113854527bfb4893cd264eec0a8b1747c3bd7
karrpp <sinceremin0718@gmail.com>
2024-12-17 18:08:28 +0900

    "fix(profile): 페이지 렌더링 오류 해결 및 최신화"

---
+++ src/components/Profile/main.js
@@ -0,0 +1,4 @@
+import '/src/components/Profile/profileComponents/profile_component';
+import '/src/components/Profile/profileUnclosed/profile_unclosed';
+import '/src/components/Profile/profileIconballons/profile_icon@balloon';
+import '/src/components/Profile/';
