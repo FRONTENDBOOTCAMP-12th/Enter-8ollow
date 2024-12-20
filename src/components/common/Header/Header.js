cb3e6bf59087c9ac706ffd01881f5c0fbde9ce6f
akman12914 <rnlduadns98@naver.com>
2024-12-20 15:57:37 +0900

    [Feat] Header 클릭하면 바뀌는 색상 + 경로 연결 (main, board)

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -2,12 +2,22 @@ import { LitElement, html } from 'lit';
 import s from '/src/components/common/Header/Header.css?inline';
 
 import fullHome from '/src/assets/fullHome.svg';
+import home from '/src/assets/home.svg';
+import fullBoard from '/src/assets/fullFile.svg';
 import board from '/src/assets/board.svg';
+import fullMap from '/src/assets/fullMap.svg';
 import map from '/src/assets/map.svg';
+import fullChat from '/src/assets/fullChat.svg';
 import chat from '/src/assets/chat.svg';
+import fullProfile from '/src/assets/fullMy.svg';
 import profile from '/src/assets/profile.svg';
 
 class BaseHeader extends LitElement {
+  static properties = {
+    checked: { type: Number },
+    index: { type: Number },
+  };
+
   constructor() {
     super();
     const shadowRoot = this.attachShadow({ mode: 'open' });
@@ -19,6 +29,11 @@ class BaseHeader extends LitElement {
     shadowRoot.appendChild(link);
   }
 
+  toggleClass(index) {
+    this.checked = index;
+    console.log(this.checked);
+  }
+
   render() {
     return html`
       <style>
@@ -26,34 +41,49 @@ class BaseHeader extends LitElement {
       </style>
       <header>
         <ul>
-          <li>
-            <a target="_blank" rel="noreferrer noopener">
-              <img alt="홈" src="${fullHome}" />
+          <li @click="${() => this.toggleClass(0)}">
+            <a href="/src/pages/main/">
+              <img
+                alt="홈"
+                src="${this.checked === 0 ? `${fullHome}` : `${home}`}"
+              />
               <p>홈</p>
               <p>${this.home}</p>
             </a>
           </li>
-          <li>
-            <a target="_blank" rel="noreferrer noopener">
-              <img alt="게시판" src="${board}" />
+          <li @click=${() => this.toggleClass(1)}>
+            <a href="/src/pages/board/board.html">
+              <img
+                alt="게시판"
+                src="${this.checked === 1 ? `${fullBoard}` : `${board}`}"
+              />
               <p>게시판</p>
             </a>
           </li>
-          <li>
+          <li @click=${() => this.toggleClass(2)}>
             <a target="_blank" rel="noreferrer noopener">
-              <img alt="내 근처" src="${map}" />
+              <img
+                alt="내 근처"
+                src="${this.checked === 2 ? `${fullMap}` : `${map}`}"
+              />
               <p>내 근처</p>
             </a>
           </li>
-          <li>
+          <li @click=${() => this.toggleClass(3)}>
             <a target="_blank" rel="noreferrer noopener">
-              <img alt="채팅" src="${chat}" />
+              <img
+                alt="채팅"
+                src="${this.checked == 3 ? `${fullChat}` : `${chat}`}"
+              />
               <p>채팅</p>
             </a>
           </li>
-          <li>
+          <li @click=${() => this.toggleClass(4)}>
             <a target="_blank" rel="noreferrer noopener">
-              <img alt="내 근처" src="${profile}" />
+              <img
+                alt="내 근처"
+                src="${this.checked === 4 ? `${fullProfile}` : `${profile}`}"
+              />
               <p>나의 이듬</p>
             </a>
           </li>
