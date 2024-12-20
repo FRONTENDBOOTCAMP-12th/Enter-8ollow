4db517201377c4ca9a0f7e52381c0bb03ba73066
akman12914 <rnlduadns98@naver.com>
2024-12-20 11:51:14 +0900

    [Feat]테마리스트의 닫기버튼과 메인버튼의 체크를 동시에 제어하는 이벤트
    - 이외 board의 사소한 오류들 수정
    - position: sticky로 변경하여 오르내릴 때 문제 해결
    - 스크롤 시 테마리스트가 따라오는 문제 해결

--- src/pages/board/board.js
+++ src/pages/board/board.js
@@ -6,6 +6,7 @@ class Board extends LitElement {
   static properties = {
     postArray: { type: Array },
     postDetailArray: {},
+    postId: { type: String },
   };
   constructor() {
     super();
@@ -13,11 +14,13 @@ class Board extends LitElement {
     this.postArray = [];
     this.postDetailArray = [];
     this.connectedCallback();
+    this.postId = 'no4l9i8q06plv7f';
   }
 
   async connectedCallback() {
     super.connectedCallback();
     await this.fetchData();
+    this.toggleHidden();
   }
 
   async fetchData() {
@@ -73,6 +76,43 @@ class Board extends LitElement {
     });
 
     this.requestUpdate();
+  }
+
+  toggleHidden() {
+    //테마리스트의 닫기버튼과 메인버튼의 체크를 동시에 제어하는 이벤트
+    const boardThemeList = document
+      .querySelector('board-page')
+      .shadowRoot.querySelector('board-theme-list');
+
+    const button = document
+      .querySelector('board-page')
+      .shadowRoot.querySelector('.category');
+
+    console.log(button);
+    console.log(close);
+
+    setTimeout(() => {
+      const closeTheme = boardThemeList
+        ? boardThemeList.shadowRoot.querySelector('.close')
+        : null;
+      const innerButton = button
+        ? button.shadowRoot.querySelector('button')
+        : null;
+
+      //닫기 버튼이 클릭되면 hidden과 button의 checked 클래스를 토글
+      if (closeTheme && innerButton) {
+        innerButton.addEventListener('click', () => {
+          boardThemeList.classList.toggle('hidden');
+        });
+
+        closeTheme.addEventListener('click', () => {
+          boardThemeList.classList.toggle('hidden');
+          innerButton.classList.toggle('checked');
+        });
+      } else {
+        console.error('dom 요소들을 찾지 못했습니다.');
+      }
+    }, 300); // 300ms 후에 중첩된 두 번째 shadowRoot에 접근
   }
 
   render() {
@@ -137,11 +177,7 @@ class Board extends LitElement {
       </style>
 
       <div class="main-button-container">
-        <main-button
-          class="category"
-          name="주제"
-          onclick="document.querySelector('board-page').shadowRoot.querySelector('board-theme-list').classList.toggle('hidden')"
-        ></main-button>
+        <main-button class="category" name="주제"></main-button>
         <main-button name="인기글"></main-button>
         <main-button name="같이해요"></main-button>
         <main-button name="질의응답"> </main-button>
