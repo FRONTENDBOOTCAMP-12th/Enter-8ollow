38bb0d974fe968ad8393a8ca0bbca6d1f5c5c568
karrpp <sinceremin0718@gmail.com>
2024-12-20 16:01:48 +0900

    feat(vite): Profile 페이지 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ const config = defineConfig({
         login: resolve(__dirname, 'src/pages/Login/index.html'),
         register: resolve(__dirname, 'src/pages/register/index.html'),
         main: resolve(__dirname, 'src/pages/main/index.html'),
+        profile: resolve(__dirname, 'src/components/Profile/index1.html'),
       },
     },
   },
