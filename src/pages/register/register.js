168ef4463322e93fe195601548b0683ea935cffe
myeong-jae-hwi <audwognl@gmail.com>
2024-12-22 16:19:48 +0900

    [BugFix]: 중간점검 수정사항 반영
    - 회원가입 시 input type tel로 변경
    - 전화번호를 입력받을 때 성능 이슈 보완

--- src/pages/register/register.js
+++ src/pages/register/register.js
@@ -77,8 +77,19 @@ class Register extends LitElement {
     const input = e.composedPath().find((el) => el.tagName === 'INPUT');
     if (!input) return;
 
-    this.phoneNumberLength = input.value.length;
+    // this.phoneNumberLength = input.value.length;
     this.phoneNumber = input.value;
+
+    this.disable = false;
+  }
+
+  handleAuthInput(e) {
+    const input = e.composedPath().find((el) => el.tagName === 'INPUT');
+    if (!input) return;
+
+    this.inputAuth = input.value;
+
+    this.authenticationDisable = false;
   }
 
   async handleRegister() {
@@ -154,16 +165,7 @@ class Register extends LitElement {
             maxlength="13"
             placeholder="휴대폰번호(- 없이 숫자만 입력)"
             type="phoneNumber"
-            @input="${(e) => {
-              const input = e
-                .composedPath()
-                .find((el) => el.tagName === 'INPUT');
-              if (!input) return;
-
-              this.phoneNumberLength = input.value.length;
-              this.phoneNumber = input.value;
-              // console.log(this.phoneNumberLength);
-            }}"
+            @input="${this.handlePhoneNumberInput}"
           ></input-component>
 
           <auth-button
@@ -176,15 +178,7 @@ class Register extends LitElement {
             ? html`<input-component
                   placeholder="인증번호 입력"
                   maxlength="6"
-                  @input="${(e) => {
-                    const input = e
-                      .composedPath()
-                      .find((el) => el.tagName === 'INPUT');
-                    if (!input) return;
-
-                    this.authenticationNumber = input.value.length;
-                    this.inputAuth = input.value;
-                  }}"
+                  @input="${this.handleAuthInput}"
                 ></input-component>
                 <p class="info">Hint. 사실 인증번호는 콘솔에 뜹니다</p>
 
