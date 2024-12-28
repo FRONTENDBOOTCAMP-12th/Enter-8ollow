ee3cb4f722e33d19d14eacccc859dd9661d7be2e
myeong-jae-hwi <audwognl@gmail.com>
2024-12-29 02:09:25 +0900

    [Style]: 오타 수정

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,3 +1,3 @@
-import {} from '/src/pages/main/exchange/exchange.js';
+import {} from '/src/pages/main/Exchange/Exchange.js';
 import {} from '/src/pages/main/SeniorStory/SeniorStory.js';
 import {} from '/src/pages/qna/QnaPage.js';
