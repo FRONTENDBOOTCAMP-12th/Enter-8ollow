43a61340e66ae6f6ee181fa81d042ae95595d674
peachily <peachily1108@gmail.com>
2024-12-31 17:46:11 +0900

    [Feat]: qnaBoard 페이지 제작 중 - 댓글 포켓베이스 연결했습니다.

--- src/pages/qnaBoard/qnaBoard.js
+++ src/pages/qnaBoard/qnaBoard.js
@@ -1,21 +1,66 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html } from 'lit';
 import { styles } from '/src/pages/qnaBoard/qnaBoardCSS.js';
+import pb from '/src/api/pocketbase';
 
 class QnaBoard extends LitElement {
   static get properties() {
     return {
       article: { type: Object },
       articleId: { type: String },
+      comments: { type: Array },
     };
   }
-
   static styles = styles;
 
   constructor() {
     super();
     this.article = null;
     this.articleId = this.getArticleIdFromUrl();
-    this.fetchArticle();
+    this.comments = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.fetchComments();
+  }
+
+  async fetchArticle() {
+    try {
+      this.article = await pb.collection('qnaPosts').getOne(this.articleId);
+      this.requestUpdate();
+    } catch (error) {
+      console.error('게시글 가져오기 실패:', error);
+    }
+  }
+
+  async fetchComments() {
+    try {
+      const rawComments = await pb.collection('comments').getFullList({
+        filter: `qnadetail='${this.articleId}'`,
+      });
+
+      this.comments = await Promise.all(
+        rawComments.map(async (comment) => {
+          const nickname = await this.getNickname(comment.user);
+          return { ...comment, nickname };
+        })
+      );
+
+      console.log('댓글:', this.comments);
+      this.requestUpdate();
+    } catch (error) {
+      console.error('댓글 가져오기 실패:', error);
+    }
+  }
+
+  async getNickname(uid) {
+    try {
+      const user = await pb.collection('members').getOne(uid);
+      return user.nickName || '';
+    } catch (error) {
+      console.error(`닉네임 가져오기 실패 (UID: ${uid}):`, error);
+      return 'Unknown User';
+    }
   }
 
   getArticleIdFromUrl() {
@@ -23,15 +68,8 @@ class QnaBoard extends LitElement {
     return urlParams.get('qnadetail');
   }
 
-  async fetchArticle() {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_PB_API}/collections/qnaPosts/records/${this.articleId}`
-      );
-      this.article = await response.json();
-    } catch (error) {
-      console.error('가져오기 실패:', error);
-    }
+  firstUpdated() {
+    this.fetchArticle();
   }
 
   render() {
@@ -39,16 +77,12 @@ class QnaBoard extends LitElement {
       return html`<p>로딩 중...</p>`;
     }
 
-    const comments = Array.isArray(this.article.comment)
-      ? this.article.comment
-      : [];
-
     return html`
       <div class="detail-container">
         <with-us></with-us>
         <div class="organizer-profile">
           <span class="sss">사진 ${this.article.ssss}</span>
-          <span class="author">이름${this.article.author}</span>
+          <span class="author">이름 ${this.article.author}</span>
           <span class="ddd">인증횟수 ${this.article.dddd}</span>
           <span class="created">• ${this.article.created}</span>
         </div>
@@ -64,25 +98,8 @@ class QnaBoard extends LitElement {
         />
       </div>
 
-      <div class="comments-section">
-        <input
-          id="comment-input"
-          class="comment-input"
-          type="text"
-          placeholder="댓글을 입력해주세요."
-        />
-        <button type="submit" class="comment-submit">추가</button>
-
-        ${comments.length === 0
-          ? html`<p class="no-comments">
-              아직 댓글이 없어요. 가장 먼저 댓글을 남겨보세요.
-            </p>`
-          : html`
-              <ul>
-                ${comments.map((comment) => html`<li>${comment.comment}</li>`)}
-              </ul>
-            `}
-      </div>
+      <send-message></send-message>
+      <comment-list .comments="${this.comments}"></comment-list>
     `;
   }
 }
