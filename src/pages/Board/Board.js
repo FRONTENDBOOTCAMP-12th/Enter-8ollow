7b4a5becfc1cbfaee3d3f236c0351fdc134ce60a
myeong-jae-hwi <audwognl@gmail.com>
2025-01-02 13:15:33 +0900

    [BugFix]: board페이지 수정

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -87,7 +87,7 @@ class Board extends LitElement {
 
   handleClick(item) {
     if (item.type === 'qna') {
-      location.href = `/src/pages/QnaDetail/?detail=${item.id}`;
+      location.href = `/src/pages/qnaBoard/?qnadetail=${item.id}`;
     } else if (item.type === 'with') {
       location.href = `/src/pages/BoardDetail/?detail=${item.id}`;
     }
@@ -189,7 +189,6 @@ class Board extends LitElement {
     }
 
     return html`
-      <region-menu></region-menu>
       <div class="main-button-container">
         <main-button class="category" name="주제"></main-button>
         <main-button name="인기글"></main-button>
@@ -227,7 +226,6 @@ class Board extends LitElement {
           `
         )}
       </ul>
-      <header-component></header-component>
       <board-theme-list id="board-theme-list" class="hidden"></board-theme-list>
     `;
   }
