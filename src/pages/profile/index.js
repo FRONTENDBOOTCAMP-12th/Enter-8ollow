019bc80f6d9255da70186b7d776f6924da1db804
akman12914 <rnlduadns98@naver.com>
2025-01-03 11:52:04 +0900

    [Rename]대소문자변경

---
+++ src/pages/profile/index.js
@@ -0,0 +1,7 @@
+import '/src/components/Atom/ProfileComponent';
+import '/src/components/Atom/ProfilePhoto';
+import '/src/components/Atom/ProfileFooter';
+import '/src/components/Molecule/ProfileIconballons/ProfileIconBalloon';
+import '/src/components/Molecule/ProfileTemperature/ProfileTemperature';
+import '/src/components/Molecule/ProfileIconballons/ProfileChatBallon';
+import '/src/components/Atom/ProfileIcon';
